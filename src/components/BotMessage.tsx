41de4395ac99fa1589e2d1d891864abb3ab0e190
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-05-15 21:36:22 +0800

    Finally, markdown

--- src/components/BotMessage.tsx
+++ src/components/BotMessage.tsx
@@ -1,10 +1,15 @@
-export default function BotMessage() {
-    return (
+import MarkdownViewer from "@/components/MarkdownViewer";
 
+export default function BotMessage() {
+    const md = '# Hello!\n---\nType some **Markdown**…\n```lua \nprint() warn() math.round()\n```'
+    return (
         <div className="flex justify-start">
             <div className="flex flex-col w-full max-w-xl bg-background border-2 rounded-lg p-4">
                 <h2 className="font-bold">bot</h2>
-                <p>Texts from bot <span className=" animate-pulse">⬛</span></p>
+                <article className="prose dark:prose-invert">
+                    <MarkdownViewer source={md} />
+                </article>
+                {/* <p>Texts from bot <span className=" animate-pulse">⬛</span></p> */}
             </div>
         </div>
     )
