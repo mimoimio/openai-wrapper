389dec6dce9b2181dc269cfd783b7881bd5c3e94
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-05-18 02:26:20 +0800

    😭😭😭

--- src/app/api/response/route.ts
+++ src/app/api/response/route.ts
@@ -1,24 +1,24 @@
-import OpenAI from "openai";
-const client = new OpenAI();
+// import OpenAI from "openai";
+// const client = new OpenAI(); 
 
 export async function GET(request: Request) {
     console.log(await request.json());
-    // const data = await request.json();
-    const response = await client.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: "Hello." }],
-    });
-    const message = response.choices[0].message.content;
+    // const response = await client.chat.completions.create({
+    //     model: "gpt-3.5-turbo",
+    //     messages: [{ role: "user", content: "Hello." }],
+    // });
+    // const message = response.choices[0].message.content;
     // console.log(response.output_text);
-    return new Response(JSON.stringify({ message }))
+    return new Response(JSON.stringify({ message: "Sorry no response for you 🤣🤣🤣" }))
 }
 
 export async function POST(request: Request) {
-    const data = await request.json();
-    const response = await client.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: data.messages,
-    })
-    const message = response.choices[0].message.content;
-    return new Response(JSON.stringify({ message }))
+    console.log(await request.json());
+    // const data = await request.json();
+    // const response = await client.chat.completions.create({
+    //     model: "gpt-3.5-turbo",
+    //     messages: data.messages,
+    // })
+    // const message = response.choices[0].message.content;
+    return new Response(JSON.stringify({ message: "Sorry no response for you 🤣🤣🤣" }))
 }
