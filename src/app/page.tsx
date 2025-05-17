f0ca0561d94194c2b6e75d2d2cb93087c5d1259c
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-05-18 02:56:46 +0800

    yeah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
 import { AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 import { Avatar } from "@/components/ui/avatar";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { ModeToggle } from "@/components/ModeToggle";
 import ChatInput from "@/components/ChatInput";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 
 export default function Home() {
   return (
     <>
       <nav className="flex justify-between items-center p-4 bg-background text-foreground border-b h-fit">
-        <Button>
-          <HamburgerMenuIcon />
-        </Button>
+        <Sheet >
+          <SheetTrigger className="bg-foreground text-background p-2 rounded-md">
+            <HamburgerMenuIcon className="" />
+          </SheetTrigger>
+          <SheetContent side="left" className="">
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your account
+                and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
         <div className="flex items-center justify-center gap-2">
           <span className="text-2xl font-bold">OpenAPI Wrapper</span>
         </div>
@@ -23,7 +34,7 @@ export default function Home() {
         </div>
 
       </nav>
-{/* 
+      {/* 
       <div className="border h-full w-full overflow-y-auto">
         <div className="max-w-2xl mx-auto flex flex-col gap-4 justify-start p-4 ">
           <h1 className="text-4xl">Content</h1>
@@ -33,6 +44,7 @@ export default function Home() {
       </div> */}
 
       <ChatInput />
+
     </>
   );
 }
