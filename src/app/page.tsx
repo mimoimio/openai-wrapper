19926c8fde5665de77d63628631b679da362204d
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-06-01 07:18:11 +0800

    😊

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import DeleteChatButton from "@/components/DeleteChatButton";
 import Header from "@/components/Header";
 import NewChatLink from "@/components/NewChatLink";
+import { Skeleton } from "@/components/ui/skeleton";
 import Link from "next/link";
 import { RecordModel } from "pocketbase";
 import { useEffect, useState } from "react";
@@ -9,12 +10,14 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [chats, setChats] = useState<RecordModel[] | null>(null)
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
     const call = async () => {
       const res = await fetch("api/chat/all", { method: "GET", cache: "no-cache" })
       const data = await res.json()
       const chats = data.chats
       setChats(chats)
+      setLoading(false)
     }
     call()
   }, [])
@@ -32,18 +35,31 @@ export default function Home() {
       <Header />
       <div className="max-w-2xl w-full mx-auto flex flex-col gap-4 p-4">
         <NewChatLink />
-        {chats &&
-          chats.map((chat) => {
-            return (
-              <div key={chat.id} className="flex">
-                <Link href={`/chat/${chat.id}`} className="flex flex-col gap-2 p-4 rounded-4xl shadow-foreground shadow-md/20 w-full">
-                  <h2 className="font-black">{chat.title}</h2>
-                  <p>{chat.created}</p>
-                </Link>
-                <DeleteChatButton chat_id={chat.id} onDelete={deleteChat} />
+        {
+          loading ?
+            <Skeleton className="flex p-4 rounded-4xl shadow-foreground shadow-md/20 w-full">
+              <div className="flex flex-col w-full gap-4">
+                <Skeleton className="flex h-1 w-1/2 p-4 shadow-foreground bg-pink-50/30 shadow-md/20  rounded-full" />
+                <Skeleton className="flex h-10 w-full shadow-foreground bg-pink-50/30 shadow-md/20 rounded-full" />
               </div>
-            )
-          })
+
+            </Skeleton>
+            :
+
+            chats &&
+            chats.map((chat) => {
+              return (
+                <div key={chat.id} className="flex">
+                  <Link href={`/chat/${chat.id}`} className="flex bg-pink-400/20 p-4 rounded-4xl shadow-foreground shadow-md/20 w-full">
+                    <div className="flex flex-col gap-2 w-full ">
+                      <h2 className="font-black">{chat.title}</h2>
+                      <p>{chat.created}</p>
+                    </div>
+                    <DeleteChatButton chat_id={chat.id} onDelete={deleteChat} />
+                  </Link>
+                </div>
+              )
+            })
         }
       </div>
     </>
--- src/components/DeleteChatButton.tsx
+++ src/components/DeleteChatButton.tsx
@@ -4,24 +4,23 @@ import { Button } from "./ui/button";
 
 export default function DeleteChatButton({ chat_id, onDelete }: { chat_id: string, onDelete(chat_id: string): void }) {
     return (
-        <div className="flex">
-            <Button onClick={async () => {
-                const response = await fetch("/api/chat",
-                    {
-                        method: "DELETE",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({
-                            chat_id: chat_id
-                        })
-                    }
-                );
-                if (response.ok) {
-                    onDelete(chat_id)
-                    console.log(response)
+        <Button onClick={async (e) => {
+            e.stopPropagation();
+            const response = await fetch("/api/chat",
+                {
+                    method: "DELETE",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        chat_id: chat_id
+                    })
                 }
-            }} className="bg-destructive text-white flex w-fit p-2 px-4 rounded-xl">
-                Delete Chat
-            </Button>
-        </div>
+            );
+            if (response.ok) {
+                onDelete(chat_id)
+                console.log(response)
+            }
+        }} className="bg-destructive text-white flex w-fit p-2 px-4 rounded-xl hover:cursor-pointer">
+            Delete Chat
+        </Button>
     )
 }
